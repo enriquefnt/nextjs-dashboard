a7915f0b22753137e16f778bbe38fba13447a31b
enrique61 <enriquefinetti@gmail.com>
2026-03-29 18:02:19 -0300

    avances

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,11 +17,11 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    // await new Promise((resolve) => setTimeout(resolve, 1500));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+   // console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -72,6 +72,7 @@ export async function fetchCardData() {
     const numberOfCustomers = Number(data[1][0].count ?? '0');
     const totalPaidInvoices = formatCurrency(data[2][0].paid ?? '0');
     const totalPendingInvoices = formatCurrency(data[2][0].pending ?? '0');
+    
 
     return {
       numberOfCustomers,
