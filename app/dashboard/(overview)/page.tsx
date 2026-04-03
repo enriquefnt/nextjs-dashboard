4d3d16556691a6014c0da36eb8ba9794928c911c
enrique61 <enriquefinetti@gmail.com>
2026-04-03 11:08:52 -0300

    avance tutorial

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,13 +3,17 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 //import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
-import {  fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import {  fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton,
+         LatestInvoicesSkeleton,
+         CardsSkeleton,
+ } from '@/app/ui/skeletons';
+ import CardWrapper from '@/app/ui/dashboard/cards';
  
 export default async function Page() {
 
-  const latestInvoices = await fetchLatestInvoices();
+ 
   const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } =
     await fetchCardData();
   return (
@@ -31,7 +35,9 @@ export default async function Page() {
            <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
